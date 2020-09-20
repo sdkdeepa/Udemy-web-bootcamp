ba000bdf217cb3f6a4bdcd694212e088bbc2d054
Deepa Subramanian <deepa94086@gmail.com>
2020-09-19 17:59:45 -0700

    Intro to Node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,10 @@
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySuperVillian = supervillains.random();
+console.log(mySuperheroName, mySuperVillian);
+
+
+// console.log(mySuperVillian);
