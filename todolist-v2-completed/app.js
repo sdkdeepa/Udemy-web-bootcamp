d870c3c40f4ffcb491b2e5fc2307b5ee48acf36b
Deepa Subramanian <deepa94086@gmail.com>
2020-09-30 01:47:05 -0700

    Edit mongoDB server details

--- todolist-v2-completed/app.js
+++ todolist-v2-completed/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-deepa:Test123@cluster0.aqegz.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin:<Password>@cluster0.aqegz.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
